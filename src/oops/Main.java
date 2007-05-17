bcf1736c583b17585921c4f1db95a56d17830143
vanore <vanore@b173c73b-982c-0410-94bc-1170376268d5>
2007-05-17 19:34:18 +0000

    Move methods from Analyzer utility class into Main.  Remove Analyzer utility class.

--- src/oops/Main.java
+++ src/oops/Main.java
@@ -29,6 +29,7 @@ import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -41,6 +42,8 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import oops.util.ConcurrentDependencyVisitor;
 
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Attribute;
@@ -210,6 +213,44 @@ public class Main implements Runnable {
         Main m = new Main(classes);
         m.visitor = visitor;
         m.run();
+    }
+    
+    /**
+     * Analyze the class path for failed dependencies.
+     * @return a set of failed class names
+     * @throws IOException if a class path entry cannot be read
+     */
+    public static Set<String> analyze() throws IOException, InterruptedException {
+        Main analyzer = new Main();
+        return getFailures(analyzer);
+    }
+    
+    /**
+     * Analyze a class or list of classes for failed dependencies.
+     * @param classes the list of classes to check
+     * @return a set of failed class names
+     */
+    public static Set<String> analyze(String... classes) throws InterruptedException {
+        Main analyzer = new Main(classes);
+        return getFailures(analyzer);
+    }
+    
+    private static Set<String> getFailures(Main analyzer) throws InterruptedException {
+        ConcurrentDependencyVisitor visitor = new ConcurrentDependencyVisitor();
+        analyzer.setDependencyVisitor(visitor);
+        
+        Thread workerThread = new Thread(analyzer);
+        workerThread.setDaemon(true);
+        workerThread.start();
+        
+        try {
+            return visitor.getFailures();
+        } catch (InterruptedException ie) {
+            //we are the only owner of the worker thread, so we need
+            //to handle the interrupt for it as well.
+            workerThread.interrupt();
+            throw ie;
+        }
     }
     
     protected void addClass(String... classes) {
